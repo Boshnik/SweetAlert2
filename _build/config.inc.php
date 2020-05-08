4bffad1e4ea496821135b32eb2ae7e353a211903
Aleksandr Huz <Superboshnik@yandex.ru>
2020-05-08 11:22:32 +0300

    Version 1.0.1

--- _build/config.inc.php
+++ _build/config.inc.php
@@ -11,7 +11,7 @@ if (!defined('MODX_CORE_PATH')) {
 return [
     'name' => 'SweetAlert2',
     'name_lower' => 'sweetalert2',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'release' => 'pl',
     // Install package to site right after build
     'install' => true,
